3eab595cc1f0d5d7fdabca4de83b4e3489093d1a
Adson Leite <adsonferreira585@gmail.com>
2024-09-11 11:28:53 -0300

    p3

--- forca.py
+++ forca.py
@@ -1,161 +1,168 @@
-# imports
-
 import random
 from os import system, name
 
-# Função para limpara tela a cada execução 
-
-def linpar_tela():
-
-    # Windows
+# metodo para limpar tela 
+def limpa_tela():
     if name == 'nt':
-        _ = system('cls')
-    
-    # Mec/Linux
+        _= system('cls')
     else:
-        _ = system('clear')
+        _= system('clear')
 
-# Função que desenha a forca na tela
-def forc(chances):
-    # Lista de estagios da forca
-    stages = [ # estagio 6(Final)
-            '''
-                --------
-                |      |
-                |      O
-                |     \\|/
-                |      |
-                |     / \\
-                -
-            ''',
-            # Estagio 5
-            '''
-                 --------
-                |      |
-                |      O
-                |     \\|/
-                |      |
-                |     / 
-                -
-            ''',
-            # Estagio 4
-               '''
-                  --------
-                |      |
-                |      O
-                |     \\|/
-                |      |
-                |     
-                -
-            ''',
-            # Estagio 3
-               '''
-                  --------
-                |      |
-                |      O
-                |     \\|
-                |      |
-                |     
-                -
-            ''',
-            # Estagio 2
-               '''
-                 --------
-                |      |
-                |      O
-                |      |
-                |      |
-                |     
-                -
-            ''',
-            # Estagio 1
-               '''
-                ------
-                |    |
-                |    O
-                |  
-                |    
-                |   
-                |
-                -
-            ''',
-            # Estagio 0
-               '''
-                ------
-                |    |
-                |    
-                |  
-                |    
-                |   
-                |
-                -
-            '''
+board = ['''
+>>>>>>>>>>Hangman<<<<<<<<<<
 
-    ]
-    return stages[chances]
++---+
+|   |
+    |
+    |
+    |
+    |
+=========''', '''
 
-def game():
++---+
+|   |
+O   |
+    |
+    |
+    |
+=========''', '''
 
-    linpar_tela()
++---+
+|   |
+O   |
+|   |
+    |
+    |
+=========''', '''
 
-    print("\nBem vindo (a) jogo da forca")
-    print("Adivinhe a palavra abaixo\n")
+ +---+
+ |   |
+ O   |
+/|   |
+     |
+     |
+=========''', '''
 
-    # lista de palavras do jogo
-    palavras = ["Abacate", "Abacaxi", "Acerola", "Açaí", "Ameixa", "Amora", "Araçá", "Banana",
-    "Bergamota", "Buriti", "Cacau", "Caju", "Cambuci", "Caqui", "Carambola", "Cereja",
-    "Coco", "Cupuaçu", "Damasco", "Figo", "Framboesa", "Goiaba", "Graviola", "Groselha",
-    "Jabuticaba", "Jaca", "Jambo", "Jatobá", "Jenipapo", "Kiwi", "Laranja", "Limão",
-    "Lichia", "Maçã", "Mamão", "Manga", "Mangaba", "Maracujá", "Melancia", "Melão",
-    "Mirtilo", "Morango", "Nectarina", "Noni", "Pequi", "Pera", "Pêssego", "Pitanga",
-    "Pitaia", "Romã", "Sapoti", "Siriguela", "Tamarindo", "Tangerina", "Tomate",
-    "Uva", "Umbu", "Veludo", "Zabumba", "Zimbro"]
+ +---+
+ |   |
+ O   |
+/|\  |
+     |
+     |
+=========''', '''
 
-    # Escolhe uma palavra da lista de forma aleatoria
-    palavra = random.choice(palavras).lower()
+ +---+
+ |   |
+ O   |
+/|\  |
+/    |
+     |
+=========''', '''
 
-    # List comprehension 
-    letras_descobertas = ['_' for letras in palavra]
+ +---+
+ |   |
+ O   |
+/|\  |
+/ \  |
+     |
+=========''']
 
-    # numero de chances
-    chances = 6
+class Hangman:
 
-    # Lista para letras erradas 
-    letras_erradas = []
+    # construtor
+    def __init__(self, palavra):
+        self.palavra = palavra
+        self.letra_erradas = []
+        self.letras_escolhidas = []
 
-    # Loop em quanto o numero de chances for maior que zero
-    while chances > 0:
-        print(forc(chances))
-        print(" ".join(letras_descobertas))
-        print("\nChances restantes:", chances)
-        print("Letras erradas:", " ".join(letras_erradas))
-
-        #tentativas 
-        tentativa = input("\nDigite uma letra: ").lower()
-
-        #condição 
-        if tentativa in palavra:
-            index = 0
-
-            for letra in palavra:
-                if tentativa == letra:
-                    letras_descobertas[index] = letra
-                index += 1
-        else:
-            # Decremento
-            chances -= 1
-            letras_erradas.append(tentativa)
-
-        # Condição final
-        if "_" not in letras_descobertas:
-            print("\nVocê venceu, a palavra era:", palavra)
-            break
+    # metodo para adivinha letra 
+    def guess (self, letra):
         
-    # Condição final "DERROTA"
-    if "_" in letras_descobertas:
-        print("\nVoce perdeu, a palavra era: ", palavra)
+        if letra in self.palavra and letra not in self.letras_escolhidas:
+            self.letras_escolhidas.append(letra)
 
-# Bloco main 
+        elif letra not in self.palavra and letra not in self.letra_erradas:
+            self.letra_erradas.append(letra)
+
+        else:
+            return False
+        
+        return True
+    
+    # metodo  para verificar de o jogo terminou
+    def hangman_over(self):
+        return self.hangman_won() or (len(self.letra_erradas) == 6)
+    
+    # metodo para verificar se o jogador venceu
+    def hangman_won(self):
+         
+        if '_' not in self.hide_palavra():
+            return True
+        return False
+    
+    # metodo para não mostra letras no board
+    def hide_palavra(self):
+
+        rnt = ''
+
+        for letra in self.palavra:
+            if letra not in self.letras_escolhidas:
+                rnt += '_'
+            else:
+                rnt += letra
+        return rnt
+        
+    
+    def print_game_status(self):
+        print(board[len(self.letra_erradas)])
+
+        print('\nPalavras: ' + self.hide_palavra())
+        print('\nLetras erradas: ', ', '.join(self.letra_erradas))
+        print('Letras corretas: ', ', '.join(self.letras_escolhidas))
+        print()
+        
+
+
+    # metodo para ler uma palavra de forma aleatoria
+def rand_palavra():
+# doc de palavras para jogo 
+    with open("palavras/palavras_200.txt","r") as file:
+            palavras = file.read().split(", ")
+            palavra = random.choice(palavras).lower()
+            return palavra
+        
+# metodo Main - Execução do Programa 
+def main():
+
+    limpa_tela()
+
+    # Crinado um obj que seleciona uma palavra de forma randomica
+    game = Hangman(rand_palavra())
+
+    # Enquanto o jogo não tiver termiando print o statis e selecione uma palavra
+    while not game.hangman_over():
+
+        # Status do Game
+        game.print_game_status()
+
+        # receba input do terminal
+        user_input = input('\nDigite uma letra: ').lower() # corrigido
+
+        # verifica se a letra digitada faz parte da palavra
+        game.guess(user_input)
+
+    game.print_game_status()
+
+        # De acordo com o status, imprime mensagem na tela para o usuário
+    if game.hangman_won():
+        print('\nParabéns! Você venceu.')
+
+    else:
+        print('\nGame over! Você perdeu.')
+        print('A palavra era ' + game.palavra)
+        
+    print('\nFoi bom jogar com você! Agora vá estudar\n')
+
+        # Executar programa
 if __name__ == "__main__":
-    game()
-    print("\nFIM\n")
+        main() 
